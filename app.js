0264c064c1d5245729ffdd13c77f3730204f39a5
Kenold Beauplan <bkenold@gmail.com>
2016-03-10 17:02:45 +0000

    add new routes

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ app.get('/', function(req, res) {
 
 app.get('/tours', function(req, res) {
     res.render('tours/index.ejs');
-})
+});
+
+app.get('/tour/:id', function(req, res) {
+    res.send('single tour page');
+});
+
+app.get('/about', function(req, res) {
+    res.send('about page');
+});
+
+app.get('/contact', function(req, res) {
+    res.send('contact page');
+});
+
+app.get('/faqs', function(req, res) {
+    res.send('FAQs page');
+});
 
 app.listen(port, host, function () {
   console.log('Viia Tours app listening on port ' + port + '!');
